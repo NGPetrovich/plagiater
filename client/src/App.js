daebb010ef221cc2e621ee267d3c480999107c34
Nikita Goncharenko <75169515+NGPetrovich@users.noreply.github.com>
2022-01-04 11:21:50 +0200

    more files added

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+
+import NavbarPage from './components/Navbar';
+import Main from "./components/Main";
+import Book from "./components/Book";
+import ThankYou from "./components/ThankYou";
 
 function App() {
+  const [page, setPage] = useState(1);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NavbarPage setPage={setPage} />
+      {page === 0 ? <Main setPage={setPage} /> : null}
+      {page === 1 ? <Book setPage={setPage} /> : null}
+      {page === 2 ? <ThankYou /> : null}
     </div>
   );
 }
